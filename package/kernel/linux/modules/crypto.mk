fbaa5dfe9effc8fb2ca85bba065dc2c90368a6da
cyx <you@example.com>
2026-09-20 01:38:28 +0800

    mediatek: fix safexcel dependency for filogic_a73

--- package/kernel/linux/modules/crypto.mk
+++ package/kernel/linux/modules/crypto.mk
@@ -475,7 +475,7 @@ $(eval $(call KernelPackage,crypto-hw-padlock))
 
 define KernelPackage/crypto-hw-safexcel
   TITLE:= MVEBU SafeXcel Crypto Engine module
-  DEPENDS:=@(TARGET_mvebu_cortexa53||TARGET_mvebu_cortexa72||TARGET_mediatek_filogic||TARGET_mediatek_mt7623) \
+  DEPENDS:=@(TARGET_mvebu_cortexa53||TARGET_mvebu_cortexa72||TARGET_mediatek_filogic||TARGET_mediatek_filogic_a73||TARGET_mediatek_mt7623) \
 	+eip197-mini-firmware +kmod-crypto-authenc +kmod-crypto-des +kmod-crypto-md5 +kmod-crypto-hmac \
 	+kmod-crypto-sha1 +kmod-crypto-sha256 +kmod-crypto-sha512
   KCONFIG:= \
